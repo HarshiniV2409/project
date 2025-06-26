aa095223ddfe7427fc012a979efd6fced2be67a5
HARSHINI VELMURUHAN <harshinivp24@gmail.com>
2025-06-26 11:01:52 +0530

    Last commit

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -91,7 +91,7 @@ const handleSubmit = async (e) => {
   };
 
   try {
-    const response = await fetch("http://localhost:5000/api/events/create", {
+    const response = await fetch("https://pro-backend-m2v3.onrender.com/api/events/create", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
